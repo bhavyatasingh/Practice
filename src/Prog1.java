e4ef2f15cf41efe22d1f916be99c28cf9f686c31
bhavyata.singh <bhavyata.singh@DDC4-L-J6ZHRT2.dir.svc.accenture.com>
2019-12-17 22:52:14 +0530

    added comment

--- src/Prog1.java
+++ src/Prog1.java
@@ -2,7 +2,7 @@
 public class Prog1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello bhavya");
+		System.out.println("Hello bhavyata ");
 
 	}
 
--- src/prac2.java
+++ src/prac2.java
@@ -7,6 +7,7 @@ public class prac2 {
 		int c;
 		c = a+b;
 		System.out.println(c);
+		System.out.println("h1");
 
 	}
 
